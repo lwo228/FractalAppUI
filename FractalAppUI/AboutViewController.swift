e52dc9228ac050c7f691d4e5cb04682aac7d4a10
cmch237 <cmch237@uky.edu>
2019-11-04 21:04:55 -0500

    Added 5 placeholder fractal explanation views, with linked TableViewController cells. Added placeholder views for true fractal displays.

--- FractalAppUI/AboutViewController.swift
+++ FractalAppUI/AboutViewController.swift
@@ -8,23 +8,41 @@
 
 import UIKit
 
-class AboutViewController: UITableViewController {
+class AboutViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
 
+    
+    @IBOutlet weak var aboutTableView: UITableView!
+    var names = ["Fractal 1", "Fractal 2", "Fractal 3", "Fractal 4", "Fractal 5", "Fractal 6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 3
+//    }
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "FractalCell1", for: indexPath)
+//        return cell
+//    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    func aboutTableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FractalCell", for: indexPath)
+    func aboutTableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = aboutTableView.dequeueReusableCell(withIdentifier: "FractalCell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
         return cell
     }
-
-    
 }
 
 
---
+++ FractalAppUI/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  FractalAppUI
+//
+//  Created by kristen on 11/4/19.
+//  Copyright © 2019 Team Name. All rights reserved.
+//
+
+import UIKit
+
+
+class aboutViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+    
+    let names = ["Fractal 1", "Fractal 2", "Fractal 3", "Fractal 4", "Fractal 5"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(names.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "FractalCell")
+        cell.textLabel?.text = names[indexPath.row]
+        cell.backgroundColor = UIColor(red:0.18, green:0.06, blue:0.36, alpha:1.0)
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor(red:0.27, green:0.09, blue:0.56, alpha:1.0)
+        cell.selectedBackgroundView = backgroundView
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0
+        {
+            self.performSegue(withIdentifier: "TableSegue1", sender: self)
+        }
+        if indexPath.row == 1
+        {
+            self.performSegue(withIdentifier: "TableSegue2", sender: self)
+        }
+        if indexPath.row == 2
+        {
+            self.performSegue(withIdentifier: "TableSegue3", sender: self)
+        }
+        if indexPath.row == 3
+        {
+            self.performSegue(withIdentifier: "TableSegue4", sender: self)
+        }
+        if indexPath.row == 4
+        {
+            self.performSegue(withIdentifier: "TableSegue5", sender: self)
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
